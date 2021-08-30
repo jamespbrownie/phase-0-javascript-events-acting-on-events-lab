dc80686f49d8ac3e9358570c6a834fe92748597a
James Brownie <jamespbrownie@gmail.com>
2021-08-30 13:59:25 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+function moveDodgerLeft(event) {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+};
+
+function moveDodgerRight(event) {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+
+  if (right < 375 ) {
+    dodger.style.left = `${right + 5}px`;
+  }
+};
+
